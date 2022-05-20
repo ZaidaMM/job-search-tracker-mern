f5c7908930f9cc9d580ce4a220a1cc507194c6ba
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-20 17:42:25 +0100

    Register user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,12 @@
-const register = (req, res) => {
-  res.send('Register user');
+import User from '../models/User.js';
+
+const register = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(500).json({ msg: 'there was an error' });
+  }
 };
 
 const login = (req, res) => {
