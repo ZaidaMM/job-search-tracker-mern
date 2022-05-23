2e14edc546a86be6776ba4e195ae5dc4603ace07
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-23 17:31:55 +0100

    Login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,9 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError } from '../errors/index.js';
+import { BadRequestError, UnauthenticatedError } from '../errors/index.js';
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
-
   if (!name || !email || !password) {
     throw new BadRequestError('Please provide all values');
   }
@@ -14,6 +13,7 @@ const register = async (req, res) => {
     throw new BadRequestError('Email already in use');
   }
   const user = await User.create({ name, email, password });
+
   const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({
     user: {
@@ -28,7 +28,25 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('Login');
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) {
+    throw UnauthenticatedError('Invalid Credentials');
+  }
+  console.log(user);
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw UnauthenticatedError('Invalid Credentials');
+  }
+
+  const token = user.createJWT();
+  user.password = undefined;
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
 
 const updateUser = async (req, res) => {
