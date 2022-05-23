5a5793b0228498c921d69adcfe1c85286c744830
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-23 12:05:53 +0100

    Cors

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,21 @@
+import { useEffect } from 'react';
+
 const Dashboard = () => {
+  const fetchData = async () => {
+    try {
+      // const response = await fetch('/data.json');
+      const response = await fetch('http://localhost:5000/');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return <div>Dashboard</div>;
 };
 export default Dashboard;
