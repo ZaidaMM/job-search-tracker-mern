ed50a3e494e677b8992704dd7a03a0fdc17d74c5
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-23 14:41:42 +0100

    Redirect after register user

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Wrapper from '../assets/wrappers/RegisterPage';
 import { Logo, FormRow, Alert } from '../components';
 import { useAppContext } from '../context/appContext';
+import { useNavigate } from 'react-router-dom';
 
 const initialState = {
   name: '',
@@ -11,10 +12,10 @@ const initialState = {
 };
 
 function Register() {
+  const navigate = useNavigate();
   const [values, setValues] = useState(initialState);
-
-  // Global state and useNavigate
-  const { isLoading, showAlert, displayAlert, registerUser } = useAppContext();
+  const { user, isLoading, showAlert, displayAlert, registerUser } =
+    useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
@@ -39,6 +40,14 @@ function Register() {
     }
     console.log(values);
   };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate('/');
+      }, 3000);
+    }
+  }, [user, navigate]);
 
   return (
     <Wrapper className='full-page'>
