22dee93a15745719de4230090beb28ce86d90f6a
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-24 07:04:05 +0100

    Navbar setup

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,44 @@
+import { useState } from 'react';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import Logo from './Logo';
 import Wrapper from '../assets/wrappers/Navbar';
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <div>Navbar</div>
+      <div className='nav-center'>
+        <button
+          className='toggle-btn'
+          onClick={() => console.log('toggle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>Dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn'
+            onClick={() => console.log('show/hide logout')}
+          >
+            <FaUserCircle />
+            John
+            <FaCaretDown />
+          </button>
+          <div className='dropdown show-dropdown'>
+            <button
+              type='button'
+              className='dropdown-btn'
+              onClick={() => console.log('logout user')}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
