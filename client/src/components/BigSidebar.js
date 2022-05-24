f3f21e87b985a127b72ecc6264252caecf4d0286
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-24 14:01:33 +0100

    Nav links setup

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,9 +1,14 @@
 import Wrapper from '../assets/wrappers/BigSidebar';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const BigSidebar = () => {
   return (
     <Wrapper>
-      <div>BigSidebar</div>
+      <div>
+        <Logo />
+        <NavLinks />
+      </div>
     </Wrapper>
   );
 };
