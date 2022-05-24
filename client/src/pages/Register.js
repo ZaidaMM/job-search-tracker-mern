cb10dc5846f3684b9348edb07a8d6014a00f39e0
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-24 04:45:46 +0100

    Login complete

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -14,7 +14,7 @@ const initialState = {
 function Register() {
   const navigate = useNavigate();
   const [values, setValues] = useState(initialState);
-  const { user, isLoading, showAlert, displayAlert, registerUser } =
+  const { user, isLoading, showAlert, displayAlert, registerUser, loginUser } =
     useAppContext();
 
   const toggleMember = () => {
@@ -34,7 +34,7 @@ function Register() {
     }
     const currentUser = { name, email, password };
     if (isMember) {
-      console.log('Already a member');
+      loginUser(currentUser);
     } else {
       registerUser(currentUser);
     }
--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ import { BadRequestError, UnauthenticatedError } from '../errors/index.js';
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
+
   if (!name || !email || !password) {
     throw new BadRequestError('Please provide all values');
   }
@@ -35,13 +36,13 @@ const login = async (req, res) => {
 
   const user = await User.findOne({ email }).select('+password');
   if (!user) {
-    throw UnauthenticatedError('Invalid Credentials');
+    throw new UnauthenticatedError('Invalid Credentials');
   }
   console.log(user);
 
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
-    throw UnauthenticatedError('Invalid Credentials');
+    throw new UnauthenticatedError('Invalid Credentials');
   }
 
   const token = user.createJWT();
