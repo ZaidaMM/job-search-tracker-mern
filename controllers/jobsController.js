0cae2df0347a3cedf85f22d548544738d9c93ed3
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-05-28 23:04:37 +0100

    Update controller and delete controller setup

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,11 @@
 import Job from '../models/Job.js';
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError, NotFoundError } from '../errors/index.js';
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError,
+} from '../errors/index.js';
+import checkPermissions from '../utils/checkPermissions.js';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -22,7 +27,8 @@ const getAllJobs = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  const { id = jobId } = req.params;
+  const { id: jobId } = req.params;
+
   const { company, position } = req.body;
 
   if (!company || !position) {
@@ -35,6 +41,15 @@ const updateJob = async (req, res) => {
     throw new NotFoundError(`No job with id ${jobId}`);
   }
 
+  // Permissions
+  // req.user.userId (is a string) === job.createdBy (is an object)
+  // throw new UnAuthorizedError('Not authorized to access this route')
+
+  // console.log(typeof req.user.userId);
+  // console.log(typeof job.createdBy);
+
+  checkPermissions(req.user, job.createdBy);
+
   const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
     new: true,
     runValidators: true,
@@ -44,7 +59,18 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  await res.send('Delete Job');
+  const { id: jobId } = req.params;
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+
+  await job.remove();
+  res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' });
 };
 
 const showStats = async (req, res) => {
